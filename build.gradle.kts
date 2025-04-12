f98bae1128c61b250ef1233d8fe70fbde94298ba
Lee Sang kyeong <sangk3758@gmail.com>
2025-04-12 14:46:43 +0900

    feat: maven 세팅 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,18 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.3.10"
     id("io.spring.dependency-management") version "1.1.7"
+    id("maven-publish")
 }
 
-group = "org.innercircle"
-version = "0.0.1-SNAPSHOT"
+val libraryArtifactId = "simple-flags-kotlin"
+val libraryVersion = "1.0.0"
 
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(17)
     }
+    withSourcesJar()
 }
 
 repositories {
@@ -19,8 +20,14 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
+    // Spring Boot 의존성 버전 관리를 위한 BOM
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:3.3.10"))
+
+    implementation("org.springframework:spring-context")
+    implementation("org.springframework.boot:spring-boot-autoconfigure")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // 테스트 의존성
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -35,3 +42,18 @@ kotlin {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+// Maven 설정
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = libraryArtifactId
+            version = libraryVersion
+            from(components["java"])
+        }
+    }
+    repositories {
+        mavenLocal()
+    }
+}
+
