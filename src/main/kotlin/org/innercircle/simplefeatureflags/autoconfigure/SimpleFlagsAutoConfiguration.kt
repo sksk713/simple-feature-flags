a68a604406977b0545fc6d8887f5dd960f1ffc51
Lee Sang kyeong <sangk3758@gmail.com>
2025-04-17 02:26:31 +0900

    feat: auto configure 관련 빈 등록 및 필요한 property 정의

---
+++ src/main/kotlin/org/innercircle/simplefeatureflags/autoconfigure/SimpleFlagsAutoConfiguration.kt
@@ -0,0 +1,61 @@
+package org.innercircle.simplefeatureflags.autoconfigure
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.annotation.PostConstruct
+import org.innercircle.simplefeatureflags.internal.FlagLoader
+import org.innercircle.simplefeatureflags.internal.FlagRegistry
+import org.innercircle.simplefeatureflags.internal.FlagReloader
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ResourceLoader
+import java.time.Clock
+
+@Configuration
+@EnableConfigurationProperties(SimpleFlagsProperties::class)
+class SimpleFlagsAutoConfiguration(
+    private val properties: SimpleFlagsProperties,
+    private val resourceLoader: ResourceLoader,
+    private val objectMapper: ObjectMapper
+) {
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    @Bean
+    @ConditionalOnMissingBean
+    internal fun simpleFlagsSystemClock(): Clock {
+        return Clock.systemUTC()
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    internal fun flagRegistry(): FlagRegistry {
+        return FlagRegistry()
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    internal fun flagLoader(): FlagLoader {
+        return FlagLoader(resourceLoader, objectMapper)
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "simple.flags", name = ["dynamic-reloading-enabled"], havingValue = "true", matchIfMissing = true)
+    @ConditionalOnMissingBean
+    internal fun flagReloader(flagLoader: FlagLoader, flagRegistry: FlagRegistry, clock: Clock): FlagReloader {
+        return FlagReloader(properties, flagLoader, flagRegistry, clock)
+    }
+
+    @PostConstruct
+    internal fun initializeFlags(flagLoader: FlagLoader, flagRegistry: FlagRegistry) {
+        log.info("Initializing Simple Flags from location: {}", properties.location)
+        try {
+            val loadedFlags = flagLoader.loadFlags(properties.location)
+            flagRegistry.updateFlags(loadedFlags)
+        } catch (e: Exception) {
+            log.error("Failed to initialize feature flags.")
+        }
+    }
+}
